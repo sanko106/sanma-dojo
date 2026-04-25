04e7edc946ba73580ff5943ae6e61da22d779ecd
sanko106 <h.ikumi.hiraide@gmail.com>
2026-04-25 10:01:48 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,34 +1,23 @@
-const CACHE = 'sanma-v3';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  'https://unpkg.com/react@18/umd/react.production.min.js',
-  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
-  'https://unpkg.com/@babel/standalone/babel.min.js',
-  'https://fonts.googleapis.com/css2?family=Zen+Kaku+Gothic+New:wght@400;500;700;900&family=DM+Mono:wght@400;500&display=swap'
-];
+// v5 - キャッシュを完全にクリアして常に最新を取得
+const CACHE = 'sanma-v5';
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {}))
-  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
+  // 古いキャッシュを全削除
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+      Promise.all(keys.map(k => caches.delete(k)))
     )
   );
   self.clients.claim();
 });
 
+// キャッシュを使わず常にネットワークから取得
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => {
-      return cached || fetch(e.request).catch(() => caches.match('./index.html'));
-    })
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
